502df5ca164e9612317e79cc874b6ed55d58331a
xiangkun.li <xiangkun.li@envision-digital.com>
2023-07-03 06:44:57 +0800

    dev: set tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+ 
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'theme-red': '#a72a2f',
+        'font-grey': '#c4c6cc',
+        'deep-grey': '#a3a4a9'
+      },
+    }
+  },
+  plugins: [],
+}
