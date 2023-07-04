8871a4e3dff9c1b90a67fdcb4a9d69e48af356d3
xiangkun.li <xiangkun.li@envision-digital.com>
2023-07-04 21:19:21 +0800

    update

--- components/EmptyCart.tsx
+++ components/EmptyCart.tsx
@@ -7,7 +7,7 @@ export const EmptyCart = () => {
 		<div>
 			<div className="h-80 flex flex-col items-center justify-center">
 				<Image src={cartImg} alt={''} width={80} />
-				<span className="rounded-full py-0.5 px-3">You don't have any items in your cart yet.</span>
+				<span className="rounded-full py-0.5 px-3">{`You don't have any items in your cart yet.`}</span>
 			</div>
 		</div>
 	)
